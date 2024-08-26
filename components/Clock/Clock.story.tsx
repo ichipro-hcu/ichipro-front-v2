e9b0404c3b12e64908e67af83be6c6bb09d4d2c4
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-08-27 02:17:24 +0900

    Add: Clock

---
+++ components/Clock/Clock.story.tsx
@@ -0,0 +1,7 @@
+import Clock from './Clock';
+
+export default {
+  title: 'Clock',
+};
+
+export const Usage = () => <Clock />;
---
+++ components/Clock/Clock.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Card } from '@mantine/core';
+
+export default function Clock() {
+    const [time, setTime] = React.useState(new Date());
+    React.useEffect(() => {
+        const interval = setInterval(() => setTime(new Date()), 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    const formattedTime = time.toLocaleTimeString();
+    const formattedDate = time.toLocaleDateString(undefined, {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        weekday: 'short',
+    });
+
+    return (
+        <Card shadow="xs" padding="xl" style={{ textAlign: 'center' }}>
+            <div style={{ fontSize: 24, marginBottom: 10 }}>{formattedTime}</div>
+            <div style={{ fontSize: 16 }}>{formattedDate}</div>
+        </Card>
+    );
+}
